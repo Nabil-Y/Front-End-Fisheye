d8efb62f5c8031b5fd17155c970794b455c1ee24
nabil-y <nabil.yassine.dev@gmail.com>
2022-03-08 12:46:11 +0100

    Add accessibility markup

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -12,25 +12,30 @@ class PhotographerFactory {
 
     createHomeHTML() {
         return `<article>
-        <a href="./photographer.html?id=${this.id}">
-            <img src="./assets/photographers/${this.portrait}" alt=${this.name}>
+        <a role="Link (h2) + image" href="./photographer.html?id=${this.id}" aria-label="${this.name}">
+            <img src="./assets/photographers/${this.portrait}" alt="${this.name} profile picture">
             <h2>${this.name}</h2>
         </a>
-        <p class="location">${this.city}, ${this.country}</p>
-        <p>${this.tagline}</p>
-        <p class="price">${this.price}€/jour</p>
+        <div role="Text paragraph" tabindex="0">
+            <p class="location">${this.city}, ${this.country}</p>
+            <p>${this.tagline}</p>
+            <p class="price">${this.price}€/jour</p>
+        </div>
         </article>`
     }
 
     createProfileHTML() {
-        return `<div>
-        <h1>${this.name}</h1>
-        <p>${this.city}, ${this.country}</p>
-        <p>${this.tagline}</p>
+        return `
+        <div>
+            <h1 tabindex="0" role="Header (h1)">${this.name}</h1>
+            <div tabindex="0" role="text">
+                <h2 >${this.city}, ${this.country}</h2>
+                <p>${this.tagline}</p>
+            </div>
         </div>
-        <button class="contact_button" id="openModal">Contactez-moi</button>
+        <button role="button" class="contact_button" id="openModal" aria-label="Contact me">Contactez-moi</button>
         <div id="photographerImage">
-            <img src="./assets/photographers/${this.portrait}" alt="${this.name}">
+            <img tabindex="0" role="Image" src="./assets/photographers/${this.portrait}" alt="${this.name} profile picture">
         </div>
         `
     }
@@ -59,10 +64,10 @@ class Image {
     createHTML() {
         return `
         <article class="card" data-date="${this.date}" data-title="${this.title}" data-likes=${this.likes}>
-        <img class="media" src="./assets/images/${getPhotographerName(this.photographerId)}/${this.src}" alt="${this.title}" />
+        <img tabindex="0" aria-label="${this.title}" role="Image link" class="media" src="./assets/images/${getPhotographerName(this.photographerId)}/${this.src}" alt="${this.title}" />
         <div class="gallery-info">
-        <h3>${this.title}</h3>
-        <div class="likes" data-liked="false">${this.likes} <i class="fa-regular fa-heart"></i></div>
+        <h2 role="Text" tabindex="0">${this.title}</h2>
+        <div tabindex="0" role="image" aria-label="likes" class="likes" data-liked="false">${this.likes} <i class="fa-regular fa-heart"></i></div>
         </div>
         </article>
         `
@@ -82,10 +87,10 @@ class Video {
     createHTML() {
         return `
         <article class="card unplayed-video" data-date="${this.date}" data-title="${this.title}" data-likes=${this.likes}>
-        <video class="media" title="${this.title}" src="./assets/images/${getPhotographerName(this.photographerId)}/${this.src}" loop ></video>
+        <video tabindex="0" aria-label="${this.title}" role="Video link" class="media" title="${this.title}" src="./assets/images/${getPhotographerName(this.photographerId)}/${this.src}" loop ></video>
         <div class="gallery-info">
-        <h3>${this.title}</h3>
-        <div class="likes" data-liked="false">${this.likes} <i class="fa-regular fa-heart"></i></div>
+        <h2 role="Text" tabindex="0">${this.title}</h2>
+        <div tabindex="0" role="image" aria-label="likes" class="likes" data-liked="false">${this.likes} <i class="fa-regular fa-heart"></i></div>
         </div>
         </article>
         `
